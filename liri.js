4ad94264f4a859cc1c285ed0090176391d82b5d9
scoslo5512 <robert.queeney@gmail.com>
2018-04-21 10:14:49 -0700

    Fixed twitter and Spotify  rest pending

--- liri.js
+++ liri.js
@@ -16,6 +16,59 @@ let action = process.argv[2];
 console.log(keys.spotify); 
 console.log(action); 
 
+// functions depending on the input
+// twitter
+function twitter(){
+
+client.get('statuses/user_timeline', {count: 10}, function(error, tweets, response) {
+    if (!error) {
+      tweets.forEach((results) => { 
+        //   need to console log text and the date time (figure out that element)
+        console.log(results.text)
+        console.log(results.created_at)
+      })
+      //console.log("these are my tweets", tweets);
+    }
+})
+
+}
+
+
+// spotify function
+function music(songName){
+
+spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+    
+    console.log(data.tracks.items); 
+    })
+}; 
+
+// omdb function
+
+function movie(movieName) {
+    request(`http://www.omdbapi.com/?t=${movieName}y=&plot=short&apikey=trilogy`, function(error, response, body) {
+
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            let movieData = {
+                "Title": blah.title,
+            }
+
+            console.log(movieData);
+        }
+
+    });
+}
+
+// bonus
+// fs.appendFile will work to move data to the .txt file
+
 // need to run different functions based off of argv
 switch (action) {
     case "my-tweets": 
@@ -23,56 +76,14 @@ switch (action) {
         break; 
     
     case "spotify-this-song":
-        music(); 
+        music(process.argv[3]); 
         break; 
     
     case "movie-this":
-        movie(); 
+        movie(process.argv[3]); 
         break; 
     
     case "do-what-it-says":
         says(); 
         break; 
     }
-
-
-
-// functions depending on the input
-// twitter
-function twitter(){
-
-client.get('statuses/user_timeline', {count: 20}, function(error, tweets, response) {
-    if (!error) {
-      console.log("these are my tweets", tweets);
-    }
-})
-
-}
-// spotify function
-function music(){
-
-spotify.search({ type: 'track', query: action }, function(err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-    
-    console.log(data); 
-    })
-}; 
-
-// omdb function
-
-function movie()
-request(`http://www.omdbapi.com/?t=${argv[4]}y=&plot=short&apikey=trilogy`, function(error, response, body) {
-
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log(response);
-  }
-});
-
-// i dont understand the 4th part of this assignment, and I dont have enough time to get a response from anyone
-// my code before this doesnt fully work
